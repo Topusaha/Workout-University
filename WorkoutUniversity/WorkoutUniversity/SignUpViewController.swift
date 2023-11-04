e3c7bfdfe67eeb5159f580006cb15b82d4170a85
Topu Saha <topusaha@Topus-MacBook-Air.local>
2023-11-04 11:58:00 -0400

    UI for login, reset, and signup is complete

--- WorkoutUniversity/WorkoutUniversity/SignUpViewController.swift
+++ WorkoutUniversity/WorkoutUniversity/SignUpViewController.swift
@@ -9,9 +9,67 @@ import UIKit
 
 class SignUpViewController: UIViewController {
 
+    var signupUsername : String = ""
+    var signupPassword : String = ""
+    var signupConfirmedPassword : String = ""
+    var signupEmail : String = ""
+    
+    @IBOutlet weak var signupUsernameTextField: UITextField!
+    
+    @IBOutlet weak var signuPasswordTextField: UITextField!
+    
+    @IBOutlet weak var signupConfirmedPasswordTextField: UITextField!
+    
+    @IBOutlet weak var signupEmailTextField: UITextField!
+    
+    
+    // sets border of a text field to red
+    func setRedBorder(for textField: UITextField) {
+            textField.layer.borderColor = UIColor.red.cgColor // Set the border color to red
+        textField.layer.borderWidth = 3.0 // Set the border width
+            textField.layer.cornerRadius = 5.0 // Add some corner radius for a nicer appearance
+    }
+    
+    // resets border
+    func resetBorder(for textField: UITextField) {
+            textField.layer.borderColor = nil // Remove the custom border color
+            textField.layer.borderWidth = 0.0 // Remove the custom border width
+            textField.layer.cornerRadius = 0.0 // Remove any corner radius
+    }
+    
+    @IBAction func createAccount(_ sender: UIButton) {
+        
+        // resets borders 
+        resetBorder(for: signupUsernameTextField)
+        resetBorder(for: signuPasswordTextField)
+        resetBorder(for: signupConfirmedPasswordTextField)
+        resetBorder(for: signupEmailTextField)
+        
+        // if passwords does not match issue warning
+        if signupConfirmedPasswordTextField.text != signupConfirmedPasswordTextField.text {
+            print("Password and Confirmed Password Does not Match")
+            setRedBorder(for : signuPasswordTextField)
+            setRedBorder(for : signupConfirmedPasswordTextField)
+        }
+        
+    
+        signupUsername = signupUsernameTextField.text ?? ""
+        signupPassword = signuPasswordTextField.text ?? ""
+        signupEmail = signupEmailTextField.text ?? ""
+        
+        // if contents are blank issue warnings
+        if signupUsername == "" { setRedBorder(for : signupUsernameTextField) }
+        if signupPassword == "" {
+            setRedBorder(for : signuPasswordTextField)
+            setRedBorder(for : signupConfirmedPasswordTextField)
+        }
+        if signupEmail == "" {
+            setRedBorder(for: signupEmailTextField)
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
     }
     
 
--- WorkoutUniversity/WorkoutUniversity/ViewController.swift
+++ WorkoutUniversity/WorkoutUniversity/ViewController.swift
@@ -9,10 +9,68 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var loginEnteredUsername : String = ""
+    var loginEnteredPasswrod : String = ""
+    
+   
+    @IBOutlet weak var loginUsernameTextField: UITextField!
+    
+    @IBOutlet weak var loginPasswordTextField: UITextField!
+    
+    
+    // sets border of a text field to red
+    func setRedBorder(for textField: UITextField) {
+            textField.layer.borderColor = UIColor.red.cgColor // Set the border color to red
+        textField.layer.borderWidth = 3.0 // Set the border width
+            textField.layer.cornerRadius = 5.0 // Add some corner radius for a nicer appearance
+    }
+    
+    // sets border
+    func resetBorder(for textField: UITextField) {
+            textField.layer.borderColor = nil // Remove the custom border color
+            textField.layer.borderWidth = 0.0 // Remove the custom border width
+            textField.layer.cornerRadius = 0.0 // Remove any corner radius
+        }
+
+    
+    @IBAction func login(_ sender: Any) {
+        loginEnteredUsername = loginUsernameTextField.text ?? ""
+        loginEnteredPasswrod = loginPasswordTextField.text ?? ""
+        
+        self.resetBorder(for: self.loginUsernameTextField)
+        self.resetBorder(for: self.loginPasswordTextField)
+        
+        if loginEnteredUsername == "" {
+            setRedBorder(for: loginUsernameTextField)
+        }
+        
+        if loginEnteredPasswrod == "" {
+            setRedBorder(for: loginPasswordTextField)
+        }
+        
+        print("Entered Username: " + loginEnteredUsername)
+        print("Entered Password: " + loginEnteredPasswrod)
+        // Send information to rest API
+        // If passes, segue to the Workout-Screen screen
+        // if fails, pop up message to try again
+    }
+    
+    
+    
+    
+    @IBAction func pressedSignIn(_ sender: UIButton) {
+        self.resetBorder(for: self.loginUsernameTextField)
+        self.resetBorder(for: self.loginPasswordTextField)
+    }
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.resetBorder(for: self.loginUsernameTextField)
+        self.resetBorder(for: self.loginPasswordTextField)
     }
-
+    
 
 }
 

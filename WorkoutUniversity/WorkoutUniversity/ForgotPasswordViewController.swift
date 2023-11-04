90d17930373529d1a0bb899974fa65697d86f930
Topu Saha <topusaha@Topus-MacBook-Air.local>
2023-11-04 10:31:29 -0400

    Signup and reset screens layed out

---
+++ WorkoutUniversity/WorkoutUniversity/ForgotPasswordViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ForgotPasswordViewController.swift
+//  WorkoutUniversity
+//
+//  Created by Topu Saha on 11/4/23.
+//
+
+import UIKit
+
+class ForgotPasswordViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    
+
+}
---
+++ WorkoutUniversity/WorkoutUniversity/SignUpViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SignUpViewController.swift
+//  WorkoutUniversity
+//
+//  Created by Topu Saha on 11/4/23.
+//
+
+import UIKit
+
+class SignUpViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+
+    
+
+}

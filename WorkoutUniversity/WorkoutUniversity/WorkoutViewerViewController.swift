c59d6d56cace90dfb7cb61d3fab834725eb53974
Topu Saha <topusaha@Topus-Air.lan>
2023-11-07 21:52:15 -0500

    Bug Fix Table View

---
+++ WorkoutUniversity/WorkoutUniversity/WorkoutViewerViewController.swift
@@ -0,0 +1,24 @@
+//
+//  WorkoutViewerViewController.swift
+//  WorkoutUniversity
+//
+//  Created by Topu Saha on 11/7/23.
+//
+
+import UIKit
+
+class WorkoutViewerViewController: UIViewController {
+
+    @IBOutlet weak var workoutViewTable: UITableView!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+    
+
+   
+
+}

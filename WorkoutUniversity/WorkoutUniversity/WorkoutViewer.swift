8eaeac2fc00a9e79555b70efa6bb5378fc40f152
Topu Saha <topusaha@Topus-Air.lan>
2023-11-07 21:38:49 -0500

    Created TableViewCell

---
+++ WorkoutUniversity/WorkoutUniversity/WorkoutViewer.swift
@@ -0,0 +1,25 @@
+//
+//  WorkoutViewer.swift
+//  WorkoutUniversity
+//
+//  Created by Topu Saha on 11/7/23.
+//
+
+import UIKit
+
+class WorkoutViewer: UITableViewCell {
+
+  
+    @IBOutlet weak var workoutTitle: UILabel!
+    
+    @IBOutlet weak var workoutType: UILabel!
+    
+    
+    @IBOutlet weak var workoutTargetMuscle: UILabel!
+    
+    
+    @IBOutlet weak var workoutDifficulty: UILabel!
+    
+    
+    
+}
